592fe34abc8768bb88d28e57b7c3b75a3763a88e
dzhumhur <dzhumhur.n@gmail.com>
2015-11-30 16:53:51 +0200

    XnaButtons

--- UtrepaU/Utrepau/Utrepalo.Game/Utrepalo.Game/Game1.cs
+++ UtrepaU/Utrepau/Utrepalo.Game/Utrepalo.Game/Game1.cs
@@ -9,6 +9,34 @@ namespace Utrepalo.Game
     /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
+        enum BState
+        {
+            HOVER,
+            UP,
+            DOWN
+        }
+
+        private const int NUMBER_OF_BUTTONS = 2,
+            login = 0,
+            register = 1,
+            BUTTON_HEIGHT = 100,
+           BUTTON_WIDTH = 150;
+
+        Color background_color;
+        Color[] button_color = new Color[NUMBER_OF_BUTTONS];
+        Rectangle[] button_rectangle = new Rectangle[NUMBER_OF_BUTTONS];
+        BState[] button_state = new BState[NUMBER_OF_BUTTONS];
+        Texture2D[] button_texture = new Texture2D[NUMBER_OF_BUTTONS];
+        double[] button_timer = new double[NUMBER_OF_BUTTONS];
+
+        bool mpressed, prev_mpressed = false;
+        int mx, my;
+        double frame_time;
+
+
+
+
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Texture2D mainCharRight;
@@ -27,7 +55,20 @@ namespace Utrepalo.Game
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            int x = Window.ClientBounds.Width / 2 - BUTTON_WIDTH / 2;
+            int y = Window.ClientBounds.Height / 2 -
+                NUMBER_OF_BUTTONS / 2 * BUTTON_HEIGHT -
+                (NUMBER_OF_BUTTONS % 2) * BUTTON_HEIGHT / 2;
+            for (int i = 0; i < NUMBER_OF_BUTTONS; i++)
+            {
+                button_state[i] = BState.UP;
+                button_color[i] = Color.White;
+                button_timer[i] = 0.0;
+                button_rectangle[i] = new Rectangle(x, y, BUTTON_WIDTH, BUTTON_HEIGHT);
+                y += BUTTON_HEIGHT;
+            }
+            IsMouseVisible = true;
+            background_color = Color.CornflowerBlue;
 
             base.Initialize();
         }
@@ -38,10 +79,15 @@ namespace Utrepalo.Game
         /// </summary>
         protected override void LoadContent()
         {
+            
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            mainCharRight = Content.Load<Texture2D>("rightMainChar");
+            //mainCharRight = Content.Load<Texture2D>("rightMainChar");
             // TODO: use this.Content to load your game content here
+            button_texture[login] =
+                Content.Load<Texture2D>(@"images/download");
+            button_texture[register] =
+                Content.Load<Texture2D>(@"images/register_now");
         }
 
         /// <summary>
@@ -75,9 +121,13 @@ namespace Utrepalo.Game
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Gold);
+            GraphicsDevice.Clear(background_color);
             spriteBatch.Begin();
-            spriteBatch.Draw(mainCharRight,new Rectangle(50,50,50,50),Color.White );
+            for (int i = 0; i < NUMBER_OF_BUTTONS; i++)
+            {
+                spriteBatch.Draw(button_texture[i],button_rectangle[i],button_color[i]);
+            }
+           // spriteBatch.Draw(mainCharRight,new Rectangle(50,50,50,50),Color.White );
             // TODO: Add your drawing code here
             spriteBatch.End();;
 
