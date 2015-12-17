8eb96c7867a230874a182dfb14e5808b27c07e13
DanielJelev <subleden@abv.bg>
2015-12-17 17:19:40 +0200

    Add Big boss Boyko in the field

---
+++ UtrepaU/Utrepalo.Game/Utrepalo.Game/GameObjects/Enemies/Boyko.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utrepalo.Game.GameObjects.Enemies
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    public class Boyko : Creature
+    {
+        private const int BoykoDefaultAttack = 250;
+        private const int BoykoDefaultHealthPoints = 1000;
+
+        public Boyko(Texture2D objTexture, Rectangle rectangle) : base(objTexture, rectangle, BoykoDefaultAttack, BoykoDefaultHealthPoints)
+        {
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(objTexture, this.Rectangle, new Rectangle(0, 0, 100, 100), Color.White);
+        }
+    }
+}
